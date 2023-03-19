d1c10bc4def16710278953e0d49b61f14d4e8b45
Ruslan <127895891+VRus3000@users.noreply.github.com>
2023-03-19 22:29:06 +0300

    231

--- stepik.py
+++ stepik.py
@@ -1,12 +1,10 @@
-s, i = input(), 0
-
-while i < len(s):
-    if s[i]=='a' or s[i]=='e':
-        print('Ага! Нашлась')
-        break
-    print(f'Текущая буква: {s[i]}')
-    i += 1
-else: print("Распечатали все буквы")
+ls = []
+for i in range(int(input())):
+    s = input()
+    if len(s) > 10:
+        s = s[0]+str(len(s)-2)+s[-1]
+    ls.append(s)
+print(*ls, sep='\n')
 
 '''
 
